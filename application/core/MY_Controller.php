ca72d1b89c3a7622c6e8f98ebd380fd39d8d28cd
Robert <robertnmoffat@gmail.com>
2016-03-29 17:37:16 -0700

    added authentication

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -48,13 +48,37 @@ class Application extends CI_Controller {
 	{
 		$choices = array();
 
-		$choices[] = array('name' => "Alpha", 'link' => '/alpha');
-		$choices[] = array('name' => "Beta", 'link' => '/beta');
-		$choices[] = array('name' => "Gamma", 'link' => '/gamma');
-                $choices[] = array('name' => "Login", 'link' => '/auth');
-                $choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		$userRole = $this->session->userdata('userRole');
+		if(strcmp($userRole, "admin") == 0){
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Beta", 'link' => '/beta');
+			$choices[] = array('name' => "Gamma",    'link' => '/gamma');
+			$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		}else if(strcmp($userRole, "user") == 0){
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Beta", 'link' => '/beta');
+			$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		}else {
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Login", 'link' => '/auth');
+		}
 		return $choices;
 	}
+        
+        function restrict($roleNeeded = null) {
+        $userRole = $this->session->userdata('userRole');
+        if ($roleNeeded != null) {
+            if (is_array($roleNeeded)) {
+                if (!in_array($userRole, $roleNeeded)) {
+                    redirect("/");
+                    return;
+                }
+            } else if ($userRole != $roleNeeded) {
+                redirect("/");
+                return;
+            }
+        }
+        }
 
 }
 
